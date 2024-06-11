96bd48ab4b610860503c4dccda0768f5834ea028
y7pWuXAq <mylullaby_@naver.com>
2024-06-11 11:09:58 +0900

    완료

--- miniproject/OpenAPI_ProJ/A_trip_to_Busan/MainWindow.xaml.cs
+++ miniproject/OpenAPI_ProJ/A_trip_to_Busan/MainWindow.xaml.cs
@@ -16,6 +16,10 @@ using Newtonsoft.Json.Linq;
 using Microsoft.Data.SqlClient;
 using CefSharp.DevTools.Page;
 using System.Data;
+using A_trip_to_Busan.Models;
+using Microsoft.VisualBasic.Logging;
+using CefSharp.DevTools.Network;
+using System.Diagnostics;
 
 namespace A_trip_to_Busan
 {
@@ -29,34 +33,82 @@ namespace A_trip_to_Busan
             InitializeComponent();
         }
 
-        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        private async void TxtSearchName_KeyDown(object sender, KeyEventArgs e)
         {
-
+            
         }
 
-        private void TxtSearchName_KeyDown(object sender, KeyEventArgs e)
+        private async void BtnSerch_Click(object sender, RoutedEventArgs e)
         {
+            string openApiUri = "https://apis.data.go.kr/6260000/RecommendedService/getRecommendedKr?serviceKey=E%2FDZS%2BYqnpd9f0TfrD2raWGtK6X1h2YNmw1Zl4mPcmQBGG9frAt6TFBVfIjCz5uqPd6q%2BVUIMrjkhvwKbeSuUw%3D%3D&resultType=json";
+            string result = string.Empty;
 
-        }
+            // WebResult, WebResponse 객체
+            WebRequest req = null;
+            WebResponse res = null;
+            StreamReader reader = null;
 
-        private void BtnSerch_Click(object sender, RoutedEventArgs e)
-        {
+            try
+            {
+                req = WebRequest.Create(openApiUri);
+                res = await req.GetResponseAsync();
+                reader = new StreamReader(res.GetResponseStream());
+                result = reader.ReadToEnd();
 
-        }
+                // await this.ShowMessageAsync("결과", result);
+            }
+            catch (Exception ex)
+            {
+                await this.ShowMessageAsync("오류", $"Open API 조회 오류 {ex.Message}");
+            }
 
-        private void BtnSaveData_Click(object sender, RoutedEventArgs e)
-        {
+            var jsonResult = JObject.Parse(result);
+            var status = Convert.ToString(jsonResult["getRecommendedKr"]["header"]["code"]);
 
+            if (status == "00")
+            {
+                var data = jsonResult["getRecommendedKr"]["item"];
+                var jsonArray = data as JArray; // json자체에서 []안에 들어간 배열데이터만 JArray 변환가능
+
+                var tinfo = new List<TInfo>();
+                foreach (var item in jsonArray)
+                {
+                    tinfo.Add(new TInfo()
+                    {
+                        Uc_id = Convert.ToInt32(item["UC_SEQ"]),
+                        Title = Convert.ToString(item["TITLE"]),
+                        Addr = Convert.ToString(item["ADDR1"]),
+                        Cate_Nm = Convert.ToString(item["CATE2_NM"]),
+                        Gugun_Nm = Convert.ToString(item["GUGUN_NM"]),
+                        Main_Title = Convert.ToString(item["MAIN_TITLE"]),
+                        Lat = Convert.ToDouble(item["LAT"]),
+                        Lng = Convert.ToDouble(item["LNG"]),
+                        Trfc_Info = Convert.ToString(item["TRFC_INFO"]),
+                        Itemcntnts = Convert.ToString(item["ITEMCNTNTS"]),
+                    });
+                }
+
+                this.DataContext = tinfo;
+                StsResult.Content = $"OpenAPI {tinfo.Count}건이 조회되었습니다.";
+            }
         }
 
         private void BtnViewData_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("하이");
+
         }
 
         private void GrdResult_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
 
+        }
+
+        private async void BtnInfoData_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(GrdResult.SelectedItem);
+            var curItem = GrdResult.SelectedItem as TInfo;
+
+            await this.ShowMessageAsync($"{curItem.Title}", $"{curItem.Itemcntnts}");
         }
     }
 }
---
+++ miniproject/OpenAPI_ProJ/A_trip_to_Busan/Models/TInfo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_trip_to_Busan.Models
+{
+    public class TInfo
+    {
+        public int Uc_id { get; set; } // 여행지 번호
+        public string Title { get; set; } // 제목
+        public string Addr { get; set; } // 주소
+        public string Cate_Nm { get; set; } // 여행 구분
+        public string Gugun_Nm { get; set; } // 구군 구분
+        public string Main_Title { get; set; } // 콘텐츠 명
+        public double Lat { get; set; } // 위도
+        public double Lng {  get; set; } // 경도
+        public string Trfc_Info { get; set; } // 교통정보
+        public string Itemcntnts { get; set; } // 상세내용
+
+        // 쿼리
+        public static readonly string INSERT_QUERY = @"INSERT INTO [dbo].[TInfo]
+                                                               ([UC_SEQ]
+                                                               ,[TITLE]
+                                                               ,[ADDR]
+                                                               ,[CATE_NM]
+                                                               ,[GUGUN_NM]
+                                                               ,[MAIN_TITLE]
+                                                               ,[LNG]
+                                                               ,[LAT]
+                                                               ,[TRFC_INFO]
+                                                               ,[ITEMCNTNTS])
+                                                         VALUES
+                                                               (@UC_SEQ
+                                                               ,@TITLE
+                                                               ,@ADDR
+                                                               ,@CATE_NM
+                                                               ,@GUGUN_NM
+                                                               ,@MAIN_TITLE
+                                                               ,@LNG
+                                                               ,@LAT
+                                                               ,@TRFC_INFO
+                                                               ,@ITEMCNTNTS)";
+
+        public static readonly string SELECT_QUERY = @"SELECT [UC_SEQ]
+                                                              ,[TITLE]
+                                                              ,[ADDR]
+                                                              ,[CATE_NM]
+                                                              ,[GUGUN_NM]
+                                                              ,[MAIN_TITLE]
+                                                              ,[LNG]
+                                                              ,[LAT]
+                                                              ,[TRFC_INFO]
+                                                              ,[ITEMCNTNTS]
+                                                          FROM [dbo].[TInfo]";
+
+    }
+}
